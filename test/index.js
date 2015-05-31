6cf817fb544edd64b2d5b77152202e1ecc0a46a3
Jordan Van Walleghem <j.vanwall@gmail.com>
2015-05-31 04:59:44 +0100

    Init tests.

---
+++ test/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Jordan on 5/31/2015.
+ */
+'use strict';
+
+const assert = require('assert');
+
+describe('Test', function () {
+    it('should work', function () {
+        assert.equal(1, 1);
+    });
+});
