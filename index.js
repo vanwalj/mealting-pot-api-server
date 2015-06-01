aaae1f2a7c4563f354488b8f5ee19d6367729f99
Jordan Van Walleghem <j.vanwall@gmail.com>
2015-06-01 01:26:06 +0100

    Added CORS Support

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ const koa   = require('koa');
 
 const httpLogger    = require('./lib/http-logger');
 const errCatcher    = require('./lib/err-catcher');
+const cors          = require('./lib/cors');
 const routes        = require('./routes');
 
 const app   = koa();
 
 app.use(httpLogger);
 app.use(errCatcher);
+app.use(cors);
 
 routes.forEach(function (route) {
     app.use(route);
