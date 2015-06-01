6abd215f8c983983574e5fb23aec5722d34256b7
Jordan Van Walleghem <j.vanwall@gmail.com>
2015-06-01 13:52:00 +0100

    Apidoc setup.

--- routes/users.js
+++ routes/users.js
@@ -10,6 +10,52 @@ const userController    = require('../controllers/user');
 
 const router    = Router();
 
+/**
+ * @api {get} /users/:id
+ * @apiVersion 0.1.0
+ * @apiName GetUser
+ * @apiGroup User
+ *
+ * @apiParam {String} id User unique id.
+ *
+ * @apiSuccess (200) {String} id id of the User.
+ * @apiSuccessExample Success-Response:
+ *      HTTP/1.1 200 OK
+ *      {
+ *          "id": "2676a6fd-a734-4639-a5cb-1f78e03fae2c"
+ *      }
+ *
+ * @apiError (404) NotFound User with the requested id not found.
+ */
+
+/**
+ * @api {post} /users Register a new user
+ * @apiVersion 0.1.0
+ * @apiName PostUser
+ * @apiGroup User
+ *
+ * @apiParam {String} email User email, must be a valid email or will throw a 400.
+ * @apiParam {String{6..}} password User password, must be at least 6 character long.
+ *
+ * @apiParamExample {json} Request-Example:
+ *      {
+ *          "email": "john@doe.com",
+ *          "password": "really secured"
+ *      }
+ *
+ * @apiSuccess (201) {String} id id of the User.
+ * @apiSuccess (201) {String} email Email of the User.
+ *
+ * @apiSuccessExample Success-Response:
+ *      HTTP/1.1 201 Created
+ *      {
+ *          "id": "2676a6fd-a734-4639-a5cb-1f78e03fae2c",
+ *          "email": "john@doe.com"
+ *      }
+ *
+ * @apiError (400) BadRequest Client error, more details within the response body.
+ * @apiError (409) Conflict Email already registered.
+ */
 router.route({
     method: 'POST',
     path: '/users',
